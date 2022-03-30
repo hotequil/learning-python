ba8ca0a80db9f4df943cc1adb61e7aa63b9e9857
hotequil <hotequil@protonmail.com>
2022-03-29 22:42:34 -0300

    feat: create sum script

---
+++ sum.py
@@ -0,0 +1,9 @@
+first_number = input("Digit the first number: ")
+print("First number is " + first_number)
+
+second_number = input("Digit the second number: ")
+print("Second number is " + second_number)
+
+final_sum = int(first_number) + int(second_number)
+
+print("The sum is " + str(final_sum))
