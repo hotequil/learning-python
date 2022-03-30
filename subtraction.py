8683fa6dfca6873badce5e965a97eca8d85d7332
hotequil <hotequil@protonmail.com>
2022-03-29 22:10:50 -0300

    feat: done subtraction

---
+++ subtraction.py
@@ -0,0 +1,5 @@
+currentYear = int(input("What's the current year: "))
+birthYear = int(input("What's your birth year: "))
+subtraction = currentYear - birthYear
+
+print("Your age should be " + str(subtraction))
