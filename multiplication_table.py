e5df19a3542cbde9cd227f53a2774a6413ffd3d6
hotequil <hotequil@protonmail.com>
2022-04-02 11:48:12 -0300

    feat: done multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,13 @@
+counter = 0
+
+while counter <= 10:
+    index = 0
+
+    while index <= 10:
+        final = counter * index
+
+        print("{} x {} = {}".format(counter, index, final))
+
+        index += 1
+
+    counter += 1
