cf01d3454a9c3ad2218e6ae4195a5e65ad185b48
hotequil <hotequil@protonmail.com>
2022-04-05 19:43:49 -0300

    feat: create a module

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import get_grade_media, which_is_the_biggest
+
+print("Your media is {}".format(get_grade_media()))
+print("Biggest is {}".format(which_is_the_biggest(5, 10)))
+print("Biggest is {}".format(which_is_the_biggest(8, 2)))
+print("Biggest is {}".format(which_is_the_biggest(6, 6)))
--- utils.py
+++ utils.py
@@ -6,9 +6,6 @@ def get_grade_media():
     return "{:.2f}".format(media)
 
 
-print("Your media is {}".format(get_grade_media()))
-
-
 def which_is_the_biggest(first_number, second_number):
     temporary = None
 
@@ -18,8 +15,3 @@ def which_is_the_biggest(first_number, second_number):
         temporary = second_number
 
     return temporary
-
-
-print("Biggest is {}".format(which_is_the_biggest(5, 10)))
-print("Biggest is {}".format(which_is_the_biggest(8, 2)))
-print("Biggest is {}".format(which_is_the_biggest(6, 6)))
