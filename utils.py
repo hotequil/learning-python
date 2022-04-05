ae184fd3de39ea9b44e55774d0733be24959c8fe
hotequil <hotequil@protonmail.com>
2022-04-05 19:32:57 -0300

    feat: done which_is_the_biggest function

--- utils.py
+++ utils.py
@@ -7,3 +7,19 @@ def get_grade_media():
 
 
 print("Your media is {}".format(get_grade_media()))
+
+
+def which_is_the_biggest(first_number, second_number):
+    temporary = None
+
+    if first_number > second_number:
+        temporary = first_number
+    elif first_number < second_number:
+        temporary = second_number
+
+    return temporary
+
+
+print("Biggest is {}".format(which_is_the_biggest(5, 10)))
+print("Biggest is {}".format(which_is_the_biggest(8, 2)))
+print("Biggest is {}".format(which_is_the_biggest(6, 6)))
