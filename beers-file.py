5a39f5e301bc757ee5b9d44dae833b5e4cacb344
hotequil <hotequil@protonmail.com>
2022-04-06 21:47:00 -0300

    feat: create a beers json

--- beers-file.py
+++ beers-file.py
@@ -11,5 +11,12 @@ beers = {
     }
 }
 
+result = json.dumps(beers, indent=4)
+
 print(f"beers: {beers}")
-print(f"dumps: {json.dumps(beers, indent=4)}")
+print(f"dumps: {result}")
+
+file = open("./beers.json", "w", encoding="UTF-8")
+
+file.write(result)
+file.close()
